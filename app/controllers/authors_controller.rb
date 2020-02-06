d6d30e9242a83f2a6be6fc70d9275a76526b3b3d
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-02-05 20:15:54 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,19 +1,45 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    set_author
   end
 
   def new
-    @author = Author.new
+    @author = Author.new(author_params)
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      #hold on to @author, because it is now full of
+      # useful error messages, and re-render the :new page, which knows how
+      # to display them alongside the user's entries.
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    set_author
+    @author.attributes = author_params
+    
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end    
   end
 
   private
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
